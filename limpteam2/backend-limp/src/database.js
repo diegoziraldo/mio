d4bb273d7d698786ce1686d5d145e63a6e35a4d5
diegoziraldo <ziraldodiego@gmail.com>
2023-04-02 18:01:53 -0300

    020423

--- limpteam2/backend-limp/src/database.js
+++ limpteam2/backend-limp/src/database.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 const URI = process.env.MONGODB_URI;
 
-mongoose.set("strictQuery", true);
-mongoose.connect(URI,() => {
-    console.log("Connected to MongoDB");
-  });
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('Conectado a la base de datos');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+connectToDatabase();
