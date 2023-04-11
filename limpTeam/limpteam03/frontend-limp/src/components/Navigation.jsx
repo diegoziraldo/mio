1580488eb33d907d2a7798bb6d100e57962ac5b7
diegoziraldo <ziraldodiego@gmail.com>
2023-04-10 21:43:18 -0300

    100423

--- limpTeam/limpteam03/frontend-limp/src/components/Navigation.jsx
+++ limpTeam/limpteam03/frontend-limp/src/components/Navigation.jsx
@@ -7,14 +7,14 @@ export default class Navigation extends Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
-          <Link className="navbar-brand" to="/">NotesApp</Link>
+          <Link className="navbar-brand" to="/">Fichas Clientes</Link>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item active">
-                <Link className="nav-link" to="/">Notas</Link>
+                <Link className="nav-link" to="/">Clientes</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/create">Crear Nota</Link>
+                <Link className="nav-link" to="/create">Crear Ficha</Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/user">Crear Cliente</Link>
