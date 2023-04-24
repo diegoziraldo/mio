5259c5fa9da937544c51e6876d0f663dc40be758
diegoziraldo <ziraldodiego@gmail.com>
2023-04-23 22:30:02 -0300

    230423

---
+++ limpTeam/limpteam03/frontend-limp/src/components/CreateProduct.jsx
@@ -0,0 +1,168 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+
+export default class CreateProducto extends Component {
+  
+state = {
+    productos: [],
+    name:'',
+    description:'',
+    categoria:'',
+    marca:'',
+    price:'',
+    img:'',
+    stock:'',
+    infoProveedor:''
+
+  }
+
+  async componentDidMount(){
+    this.getProductos();
+  }
+  
+  getProductos = async()=>{
+    const res = await axios.get('http://localhost:3000/api/productos');
+    this.setState({productos:res.data})
+    console.log(this.state.productos);
+  }
+
+  onChangeClienteName = (e)=>{
+    this.setState({
+       name: e.target.value
+   })
+ }
+  onChangeClienteLastName = (e)=>{
+    this.setState({
+     lastname: e.target.value
+    })
+  }
+
+  onChangeClienteEmail = (e)=>{
+    this.setState({
+      email: e.target.value
+    })  
+  }
+
+  onChangeClienteAddress = (e)=>{
+    this.setState({
+      direccion: e.target.value
+    })
+  }
+
+  onChangeClienteLocalidad = (e)=>{
+    this.setState({
+      localidad: e.target.value
+    })
+  }
+
+  onChangeClienteTelephone = (e)=>{
+    this.setState({
+      telephono: e.target.value
+    })
+  }
+
+
+
+
+  onSubmit = async(e)=>{
+    e.preventDefault();
+    const newCliente = {
+      name: this.state.name,
+      lastname: this.state.lastname,
+      email: this.state.email,
+      direccion: this.state.direccion,
+      localidad: this.state.localidad,
+      telephono: this.state.telephono
+    }
+    await axios.post('http://localhost:3000/api/clientes', newCliente)
+    window.location.href = '/';
+
+    this.setState({name: ''})
+    this.setState({lastname: ''})
+    this.setState({email: ''})
+    this.setState({direccion: ''})
+    this.setState({localidad: ''})
+    this.setState({telephono: ''})
+    this.getClientes();
+
+  }
+
+  deleteUser = async(id)=>{
+    await axios.delete(`http://localhost:3000/api/clientes/${id}`);
+    this.getClientes()
+    console.log(id);
+  }
+
+
+  
+  render() {
+    return (
+      <div>
+        <form className="bg-light p-4 rounded" onSubmit={this.onSubmit}>
+        <h1>Formulario Cliente</h1>
+          <div className="form-group">
+            <label htmlFor="name">Nombre:</label>
+            <input 
+              type="text" 
+              className="form-control"
+              value={this.state.name}
+              onChange={this.onChangeProductoName}/>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="description">Descrpcion:</label>
+            <input 
+              type="text" 
+              className="form-control"
+              value={this.state.description}
+              onChange={this.onChangeDescrption}/>
+              
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input  
+              type="email" 
+              className="form-control"
+              value={this.state.email}
+              onChange={this.onChangeClienteEmail}/>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="address">Direccion:</label>
+            <input 
+              type="text" 
+              className="form-control" 
+              value={this.state.direccion}
+              onChange={this.onChangeClienteAddress}
+              />
+          </div>     
+          <div className="form-group">
+            <label htmlFor="address">Localidad:</label>
+            <input 
+              type="text" 
+              className="form-control" 
+              value={this.state.localidad}
+              onChange={this.onChangeClienteLocalidad}
+              />
+          </div> 
+          <div className="form-group">
+            <label htmlFor="address">Telefono:</label>
+            <input 
+              type="number" 
+              className="form-control" 
+              value={this.state.telephono}
+              onChange={this.onChangeClienteTelephone}
+              />
+          </div>  
+          <button type="submit" className="btn btn-primary">
+            Guardar
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+
