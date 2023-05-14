d6df6c489e5e3337739104f54768b3d1e611ca7d
diegoziraldo <ziraldodiego@gmail.com>
2023-05-14 11:52:40 -0300

    140523

--- Python/Dalto/metodos/metoldos_cadena.py
+++ Python/Dalto/metodos/metoldos_cadena.py
@@ -1,12 +1,22 @@
-cadena1 = 'holas'
+cadena1 = 'Hola'
 cadena2 = 'Bienvenido'
 
 
 #el funcionamiento de un metodo es 'DATO.NOMBRE DEL METODO()'
 #En este caso estamos pasando un string a mayusculas con el metodo 'upper()'
 
-resultado = cadena1.upper()
-resultado = cadena1.lower()
-resultado = cadena1.capitalize()
+#resultado = cadena1.upper()
+#resultado = cadena1.lower()
+#resultado = cadena1.capitalize()
+
+
+#busqueda con metodo 'find()', si no lo encuentra devovlera -1
+#resultado = cadena1.find('a')
+
+#busqueda con metodo 'find()', es igual que el metodo find(), si no hay coincidencias envia una ecepcion
+#resultado = cadena1.index('a')
+
+#metodo isnumeric(), nos devolvera un valor buleano, true/false
+resultado = cadena1.isnumeric()
 
 print(resultado)
